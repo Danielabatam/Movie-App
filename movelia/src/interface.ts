9a4a067f4c616a9a444042cf0652eccba10da701
danielabatam <danielabatam@gmail.com>
2023-06-07 10:11:03 +0100

    [FEAT]: INTERFACE - CustomComponentsProps - Season - Film

---
+++ movelia/src/interface.ts
@@ -0,0 +1,20 @@
+import React, {ReactNode} from 'react';
+
+export interface CustomComponentsProps {
+    children?:ReactNode
+    className?:string
+}
+
+export interface Season {
+    id: number
+}
+
+export interface Film {
+    id: number,
+    title: string
+    description: string
+    posterPath: string
+    coverPath: string
+    genreIds: number[]
+    seasons: Season[]
+}
